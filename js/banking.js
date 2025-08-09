6a76d90d2d85cdcba100e6d694d227f4842813f7
Shorif4321 <shorifali4321@gmail.com>
2025-08-09 23:11:41 +0600

    deposite done

---
+++ js/banking.js
@@ -0,0 +1,41 @@
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const inputField = document.getElementById('deposit-input');
+    const inputText = inputField.value;
+    const inputAmount = parseInt(inputText)
+
+    if(inputField.value == ''){
+       return alert("Valo hoye jaw masud")
+    }
+    if(inputAmount < 1){
+       return alert("Min Value is 1")
+    }
+
+    // console.log(typeof(inputAmount));
+    
+
+
+
+    const depositTotalInput = document.getElementById('deposit-total');
+    const depositTotalText = depositTotalInput.innerText;
+    const previousDeposit = parseInt(depositTotalText);
+    const newDeposit = inputAmount + previousDeposit;
+
+    depositTotalInput.innerText = newDeposit;
+
+    // balance update
+    const balanceField =  document.getElementById('balace-input');
+    const balanceText = balanceField.innerText;
+    const previousBalance = parseInt(balanceText);
+
+    const newBalance = previousBalance + inputAmount;
+    balanceField.innerText = newBalance
+
+
+    // clear
+    inputField.value = ''
+
+
+
+
+
+})
--- js/login.js
+++ js/login.js
@@ -1,11 +1,10 @@
 document.getElementById('submit-button').addEventListener('click',function(){
    const userEmailField = document.getElementById('user-email');
    const email = userEmailField.value;
-   console.log(email);
-
+   
    const userPasswordField = document.getElementById("user-password");
    const password = userPasswordField.value;
-   console.log(password);
+
    if(email =="admin@gmail.com" && password == 123456){
         window.location.href = "../banking.html";
    }
